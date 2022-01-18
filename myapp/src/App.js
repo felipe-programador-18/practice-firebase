d5bffff5514b333e5365c4ebc303a9e6c4965fb7
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-18 15:53:09 -0300

    c3- started create new usereducer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,33 +1,19 @@
 import React, {useEffect, useReducer} from 'react';
 import axios from 'axios'
 
-
 const Url = 'https://testeapp-practice-default-rtdb.firebaseio.com/movimentacoes/2022-01.json'
-
 //now i get started create usereducer  theorical he receive 2 parameter
 
 const reducer = (state, action) => {
- 
-  //handling stage
- //how Use reducer
 
- if(action.type === 'REQUEST'){
-   return {
-     ...state,
-     loading:true
-   }
- }
 
- if(action.type ==='SUCCESS'){
-   return {
-     ...state,
-     loading:false,
-     data: action.data
-   }
- }
+
+
+
 
   return state
 }
+
 
 function App() {
   //now i have setflag the date to seeing in the dashboard!!!
@@ -35,28 +21,22 @@ function App() {
   // this example of bottom is about if wants adding a words loading...
   
   //here i am handling
-  const [data, dispacth] = useReducer(reducer,{
-    loading: true,
-    data: {}
-  } )
-
   useEffect(() => {
-   
-    dispacth({type:'REQUEST'})
+
     axios
     .get(Url)
     .then(res => {
-        dispacth({type:'SUCCESS', data: res.data})}
-        )
+      console.log(res)
+    })
 
   }, [])
 
   
+
+  
   return (
     <div className="App">
-     <h1>My money test Api</h1> 
-     {JSON.stringify(data)}
-     {data.loading && <p>Loading .....</p>}
+    <h1>try it about api</h1> 
     
     </div>
   );
