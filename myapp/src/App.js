f0895a8ef27d96ba90a53f7a9630582f8939976d
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-20 16:47:36 -0300

    c7- create againg api REST with useReducer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,57 +1,52 @@
 import React, {useEffect, useReducer} from 'react';
 import axios from 'axios'
 
-const Url = 'https://testeapp-practice-default-rtdb.firebaseio.com/movimentacoes/2022-01.json'
-//now i get started create usereducer  theorical he receive 2 parameter
 
-const reducer = (state, action) => {
+const Url = "https://testeapp-practice-default-rtdb.firebaseio.com/movimentacoes.json"
 
- if(action.type === 'REQUEST'){
-   return{  ...state,
-    loading: true 
+const reducer = (state,action) =>{
+
+if(action.type === 'REQUEST'){
+return {
+  ...state ,
+  loading: true
+}}
+
+if(action.type === 'SUCCESS'){
+  return {
+    ...state,
+    loading: false,
+    data: action.data
   }
- }
-
- if(action.type ==='SUCCESS'){
-   return {
-     ...state ,
-     loading : false,
-     data: action.data
-   }
- }
-
-
-
+}
   return state
 }
 
+// here always flag date and dispatch
+// now i have setflag use usereducer
 
-function App() {
-  const [data, dispatch] = useReducer(reducer , {
+
+
+function App(){  
+  const [data, dispatch] = useReducer(reducer, {
     loading: true,
-    data : {}
+    data: {}
   })
-  
-  //here i am handling
-  useEffect(() => {
-    dispatch ({type: 'REQUEST'})
-    axios
-    .get(Url)
-    .then(res => {
-      dispatch({type:'SUCCESS', data: res.data})
-    })
 
-  }, [])
+  useEffect(() =>{
+    dispatch({type:'REQUEST'})
+   axios
+   .get(Url)
+   .then(res =>{
+     dispatch({type: 'SUCCESS', data: res.data})
+   })
+ }, [])
 
-  
-
-  
   return (
     <div className="App">
     <h1>try it about api</h1> 
-    {JSON.stringify(data)}
-    {data.loading && <p> Loading ... </p>}
-    
+    {JSON.stringify(data) }
+     {data.loading && <p>loading</p>}
     </div>
   );
 }
