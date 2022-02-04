ff971dde8ae5fc804453c84fdc612c0d05839d58
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-04 15:13:05 -0300

    c15-adding new somethings

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import UseGet from './Useget';
+import React, {useEffect, useReducer} from 'react';
+import axios from 'axios'
 
 
 const Url = "https://testeapp-practice-default-rtdb.firebaseio.com/movimentacoes.json"
+
+
+// i always create new function about reduce he receive two state (state and action) always remember this!!!
+const reducer = (state,action) =>{
+  if(action.type === 'REQUEST'){
+  return {
+    ...state ,
+    loading: true
+  }}
+  
+  if(action.type === 'SUCCESS'){
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
+    return state
+  }
+
+  const UseGet = Url => {
+    // when i set data i need invoke data and dispatch always
+    // the dispatch he always going dispatch two way request and success
+    const [data, dispatch] = useReducer(reducer, {loading: true,data: {}})
+        useEffect(() =>{
+          dispatch({type:'REQUEST'})
+         axios
+         .get(Url)
+         .then(res =>{
+           dispatch({type: 'SUCCESS', data: res.data})
+         })
+       }, [])
+       return data
+  }
+
 
 function App(){  
   //is pure functions !!
